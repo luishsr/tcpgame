0ec33f90fd45cc33cfd901fc4e50184d0b15f547
luishsr <luishsoares@gmail.com>
2023-04-26 19:07:42 -0300

    Initial commit

---
+++ client.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	tcp "github.com/luishsr/tcpconnector"
+)
+
+func main() {
+	fmt.Println("Client")
+
+	//tcp.Connect()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/luishsr/tcpgame
+
+go 1.19
+
+require github.com/luishsr/tcpconnector v0.0.0-20230426170930-9d84fea5be1f
